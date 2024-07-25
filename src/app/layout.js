bc7af96d9c20c4ec77a4e5af8001dcc1f0621382
CodeDiego15 <andrebermudezdiego@gmail.com>
2024-07-24 21:32:12 -0400

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fnews_2495007&psig=AOvVaw26fI-UJGYeITT4R1CZO-R-&ust=1721957487953000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKjGhtuFwYcDFQAAAAAdAAAAABAO" />
       <body className={inter.className}>{children}</body>
     </html>
   );
